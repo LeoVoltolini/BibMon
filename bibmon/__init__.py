51f6c100275bb03ced7b209ade373ccf5511d36e
Leonardo Voltolini <voltolini@eq.ufrj.br>
2025-03-03 10:42:36 +0100

    Update __init__.py

--- bibmon/__init__.py
+++ bibmon/__init__.py
@@ -4,12 +4,13 @@ from ._esn import ESN
 from ._sbm import SBM
 from ._sklearn_regressor import sklearnRegressor
 from ._sklearn_manifold import sklearnManifold
+from .__sklearn_classifier import sklearnClassifier
 from ._preprocess import PreProcess
 from ._load_data import load_tennessee_eastman, load_real_data
 from ._bibmon_tools import train_val_test_split, complete_analysis, comparative_table, targets_comparative_table, spearmanr_dendrogram, create_df_with_dates, create_df_with_noise, align_dfs_by_rows
 
 __all__ = ['Autoencoder','PCA','ESN','SBM',
-	         'sklearnRegressor','sklearnManifold' , 
+	         'sklearnRegressor','sklearnManifold' , 'sklearnClassifier',
            'PreProcess','load_tennessee_eastman', 
            'load_real_data', 'train_val_test_split', 
            'complete_analysis', 'comparative_table',
